ec6a5fdf53aa199f06f6304f574e23aab7e88b2e
chrisloze <52135736+chrisloze@users.noreply.github.com>
2022-10-30 03:48:45 +0400

    Update index.js

--- index.js
+++ index.js
@@ -32,6 +32,9 @@ const parse = require ("parse-ms");
 const probot = require("probot-tax");
 const db = require('quick.db');
 
+const { Client, Intents,MessageEmbed } = require('discord.js');
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_BANS, Intents.FLAGS.GUILD_MEMBERS, Intents.FLAGS.GUILD_EMOJIS_AND_STICKERS, Intents.FLAGS.GUILD_INTEGRATIONS, Intents.FLAGS.GUILD_WEBHOOKS, Intents.FLAGS.GUILD_INVITES, Intents.FLAGS.GUILD_VOICE_STATES, Intents.FLAGS.GUILD_PRESENCES, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS, Intents.FLAGS.GUILD_MESSAGE_TYPING, Intents.FLAGS.DIRECT_MESSAGES, Intents.FLAGS.DIRECT_MESSAGE_REACTIONS, Intents.FLAGS.DIRECT_MESSAGE_TYPING] });
+
 
 /////////////////
 ///ممنوع السرقة youssefgames
